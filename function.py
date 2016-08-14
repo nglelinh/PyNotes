c0066b465116b619bae64bb6f02ea321d5008e07
Acheron <acheron.igor.b@gmail.com>
2016-08-15 02:45:51 +0300

    Fixed unicode support; Rewrited function to check file availability. Now works with Python 3

--- function.py
+++ function.py
@@ -1,16 +1,16 @@
-# coding=utf-8
 import markdown
+import os
 
 
-def markdown_to_html(filename, page_folder):
+def markdown_to_html(file):
     """
     Преобразование markdown файла в html код
     """
-    f = open(page_folder + '/' + filename + '.md', 'r')
-    all_file = ''
-    for st in f.readlines():
-        all_file += st
-    html = markdown.markdown(all_file.decode('utf8'), extensions=['codehilite'])
+    with open(file, 'r', encoding='utf-8') as f:
+        all_file = ''
+        for st in f.readlines():
+            all_file += st
+    html = markdown.markdown(all_file, extensions=['codehilite'])
     return html
 
 
@@ -18,10 +18,8 @@ def check_found_file(filename):
     """
     Проверка существования файла
     """
-    try:
-        file = open(filename)
-    except IOError as e:
-        return False
-    else:
-        with file:
+    if os.access(filename, os.F_OK):
+        if os.access(filename, os.R_OK):
             return True
+    return False
+
